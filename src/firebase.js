96bfeee382e6c6536aad00279140a6271d4d328a
El coco <jesussanjuan23@gmail.com>
2022-06-05 15:45:38 -0500

    Configurando Firebase

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyATjp9ZxkHlFWlaWo0uNKk9iH3vwB4YBaM",
+    authDomain: "app-api-a3dc6.firebaseapp.com",
+    projectId: "app-api-a3dc6",
+    storageBucket: "app-api-a3dc6.appspot.com",
+    messagingSenderId: "354196373909",
+    appId: "1:354196373909:web:49aa05b8fe634f7530f69c"
+};
+
+firebase.initializeApp(firebaseConfig);
+export{firebase} 
